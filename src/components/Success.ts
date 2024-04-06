d5958eb3f650f34560a12c502047041f9db76a20
Stepan Sukhachev <droiderlinux@yahoo.com>
2024-04-06 18:21:01 +0300

    refactor: move Basket and Success from common to components

--- src/components/Success.ts
+++ src/components/Success.ts
@@ -1,6 +1,6 @@
-import { View } from '../base/Component';
-import { ensureElement } from '../../utils/utils';
-import { IEvents } from '../base/Events';
+import { View } from './base/Component';
+import { ensureElement } from '../utils/utils';
+import { IEvents } from './base/Events';
 
 interface ISuccess {
 	total: number;
